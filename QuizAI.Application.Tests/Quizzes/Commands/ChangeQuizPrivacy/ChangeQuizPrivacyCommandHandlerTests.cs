b456ec498a7544b3dcb22b564c1df1c3e50006ac
BOOMBERT <norbert.domagala2005@gmail.com>
2025-03-05 16:31:02 +0100

    Add tests for ChangeQuizPrivacyCommandHandler and DeleteQuizCommandHandler

---
+++ QuizAI.Application.Tests/Quizzes/Commands/ChangeQuizPrivacy/ChangeQuizPrivacyCommandHandlerTests.cs
@@ -0,0 +1,91 @@
+﻿using Moq;
+using QuizAI.Application.Interfaces;
+using QuizAI.Domain.Entities;
+using QuizAI.Domain.Exceptions;
+using QuizAI.Domain.Repositories;
+using Xunit;
+
+namespace QuizAI.Application.Quizzes.Commands.ChangeQuizPrivacy.Tests;
+
+public class ChangeQuizPrivacyCommandHandlerTests
+{
+    private readonly Mock<IRepository> _repositoryMock;
+    private readonly Mock<IQuizService> _quizServiceMock;
+    private readonly Mock<IImagesRepository> _imagesRepositoryMock;
+    private readonly Mock<IImageService> _imageServiceMock;
+
+    private readonly ChangeQuizPrivacyCommandHandler _handler;
+
+    public ChangeQuizPrivacyCommandHandlerTests()
+    {
+        _repositoryMock = new Mock<IRepository>();
+        _quizServiceMock = new Mock<IQuizService>();
+        _imagesRepositoryMock = new Mock<IImagesRepository>();
+        _imageServiceMock = new Mock<IImageService>();
+
+        _handler = new ChangeQuizPrivacyCommandHandler(_repositoryMock.Object, _quizServiceMock.Object, _imagesRepositoryMock.Object, _imageServiceMock.Object);
+    }
+
+    [Theory]
+    [InlineData(true, true)]
+    [InlineData(false, false)]
+    public async Task Handle_WhenValidCommand_ShouldChangeQuizPrivacy(bool isPrivate, bool createdNewQuiz)
+    {
+        // Arrange
+
+        var command = new ChangeQuizPrivacyCommand() { IsPrivate = isPrivate };
+        command.SetId(Guid.NewGuid());
+
+        var quizToUpdate = new Quiz() { Id = createdNewQuiz ? Guid.NewGuid() : command.GetId(), IsPrivate = !isPrivate };
+
+        _quizServiceMock
+            .Setup(s => s.GetValidOrDeprecateAndCreateWithNewQuestionsAsync(command.GetId()))
+            .ReturnsAsync((quizToUpdate, createdNewQuiz));
+
+        // Act
+
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+
+        Assert.Equal(result.latestQuizId, quizToUpdate.Id);
+        Assert.Equal(command.IsPrivate, quizToUpdate.IsPrivate);
+
+        if (createdNewQuiz)
+        {
+            _repositoryMock.Verify(r => r.AddAsync(quizToUpdate), Times.Once);
+        }
+        else
+        {
+            _repositoryMock.Verify(r => r.AddAsync(quizToUpdate), Times.Never);
+        }
+
+        _repositoryMock.Verify(r => r.SaveChangesAsync(), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task Handle_WhenTheSamePrivacy_ShouldThrowConflictException(bool isPrivate)
+    {
+        // Arrange
+
+        var command = new ChangeQuizPrivacyCommand() { IsPrivate = isPrivate };
+        command.SetId(Guid.NewGuid());
+
+        var quizToUpdate = new Quiz() { Id = command.GetId(), IsPrivate = isPrivate };
+
+        _quizServiceMock
+            .Setup(s => s.GetValidOrDeprecateAndCreateWithNewQuestionsAsync(command.GetId()))
+            .ReturnsAsync((quizToUpdate, false));
+
+        // Act
+
+        Func<Task> action = async () => await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+
+        var exception = await Assert.ThrowsAsync<ConflictException>(action);
+        Assert.Equal($"The privacy setting of quiz with ID {command.GetId()} is already set to {command.IsPrivate}", exception.Message);
+    }
+}
---
+++ QuizAI.Application.Tests/Quizzes/Commands/DeleteQuiz/DeleteQuizCommandHandlerTests.cs
@@ -0,0 +1,143 @@
+﻿using Moq;
+using QuizAI.Application.Interfaces;
+using QuizAI.Domain.Constants;
+using QuizAI.Domain.Entities;
+using QuizAI.Domain.Exceptions;
+using QuizAI.Domain.Repositories;
+using Xunit;
+
+namespace QuizAI.Application.Quizzes.Commands.DeleteQuiz.Tests;
+
+public class DeleteQuizCommandHandlerTests
+{
+    private readonly Mock<IRepository> _repositoryMock;
+    private readonly Mock<IQuizAuthorizationService> _quizAuthorizationServiceMock;
+    private readonly Mock<IQuizzesRepository> _quizzesRepositoryMock;
+    private readonly Mock<IQuizAttemptsRepository> _quizAttemptsRepositoryMock;
+    private readonly Mock<ICategoryService> _categoryServiceMock;
+    private readonly Mock<IImageService> _imageServiceMock;
+    private readonly Mock<IQuizPermissionsRepository> _quizPermissionsRepositoryMock;
+
+    private readonly DeleteQuizCommandHandler _handler;
+
+    public DeleteQuizCommandHandlerTests()
+    {
+        _repositoryMock = new Mock<IRepository>();
+        _quizAuthorizationServiceMock = new Mock<IQuizAuthorizationService>();
+        _quizzesRepositoryMock = new Mock<IQuizzesRepository>();
+        _quizAttemptsRepositoryMock = new Mock<IQuizAttemptsRepository>();
+        _categoryServiceMock = new Mock<ICategoryService>();
+        _imageServiceMock = new Mock<IImageService>();
+        _quizPermissionsRepositoryMock = new Mock<IQuizPermissionsRepository>();
+
+        _handler = new DeleteQuizCommandHandler(
+            _repositoryMock.Object, _quizAuthorizationServiceMock.Object, _quizzesRepositoryMock.Object, _quizAttemptsRepositoryMock.Object, 
+            _categoryServiceMock.Object, _imageServiceMock.Object, _quizPermissionsRepositoryMock.Object);
+    }
+
+    [Theory]
+    [InlineData(true, true)]
+    [InlineData(false, false)]
+    public async Task Handle_WhenValidCommand_ShouldDeleteOrDeprecateQuizAndRemoveImages(bool withImage, bool hasQuizAttempts)
+    {
+        // Arrange
+
+        var command = new DeleteQuizCommand();
+        command.SetId(Guid.NewGuid());
+
+        var questionImageId = Guid.NewGuid();
+        var quiz = new Quiz() 
+        { 
+            Id = command.GetId(),
+            ImageId = withImage ? Guid.NewGuid() : null,
+            Questions = new List<Question>
+            {
+                new Question { ImageId = questionImageId }
+            }
+        };
+
+        _quizzesRepositoryMock
+            .Setup(r => r.GetWithQuestionsAndCategoriesAsync(command.GetId()))
+            .ReturnsAsync(quiz);
+
+        _quizAttemptsRepositoryMock
+            .Setup(r => r.HasAnyAsync(quiz.Id, null, null))
+            .ReturnsAsync(hasQuizAttempts);
+
+        // Act
+
+        await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+
+        Assert.Null(quiz.ImageId);
+
+        if (hasQuizAttempts) 
+        {
+            Assert.True(quiz.IsDeprecated);
+            _quizPermissionsRepositoryMock.Verify(r => r.DeletePermissionsAsync(quiz.Id), Times.Once);
+            Assert.Contains(quiz.Questions, qn => qn.ImageId == questionImageId);
+        }
+        else
+        {
+            _repositoryMock.Verify(r => r.Remove(quiz), Times.Once);
+            _quizzesRepositoryMock.Verify(r => r.UpdateLatestVersionIdAsync(quiz.Id, null), Times.Once);
+            Assert.True(quiz.Questions.All(qn => qn.ImageId == null));
+        }
+
+        _quizAuthorizationServiceMock.Verify(s => s.AuthorizeAsync(quiz, null, ResourceOperation.Delete), Times.Once);
+        _repositoryMock.Verify(r => r.SaveChangesAsync(), Times.Exactly(2));
+    }
+
+    [Fact]
+    public async Task Handle_WhenNonExistingQuiz_ShouldThrowNotFoundException()
+    {
+        // Arrange
+
+        var command = new DeleteQuizCommand();
+        command.SetId(Guid.NewGuid());
+
+        _quizzesRepositoryMock
+            .Setup(r => r.GetWithQuestionsAndCategoriesAsync(command.GetId()))
+            .ReturnsAsync((Quiz?)null);
+
+        // Act
+
+        Func<Task> action = async () => await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+
+        var exception = await Assert.ThrowsAsync<NotFoundException>(action);
+        Assert.Equal($"Quiz with ID {command.GetId()} was not found", exception.Message);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task Handle_WhenDeprecatedQuiz_ShouldThrowNotFoundQuizWithVersioningException(bool withLatestVersionId)
+    {
+        // Arrange
+
+        var command = new DeleteQuizCommand();
+        command.SetId(Guid.NewGuid());
+
+        var quiz = new Quiz() { Id = command.GetId(), IsDeprecated = true, LatestVersionId = withLatestVersionId ? Guid.NewGuid() : null };
+
+        _quizzesRepositoryMock
+            .Setup(r => r.GetWithQuestionsAndCategoriesAsync(command.GetId()))
+            .ReturnsAsync(quiz);
+
+        // Act
+
+        Func<Task> action = async () => await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+
+        var exception = await Assert.ThrowsAsync<NotFoundQuizWithVersioningException>(action);
+        Assert.Equal($"The quiz with ID {quiz.Id} was not found - " + 
+            (withLatestVersionId 
+            ? $"The latest version ID is {quiz.LatestVersionId}" 
+            : "No latest version is available"), 
+            exception.Message);
+    }
+}
